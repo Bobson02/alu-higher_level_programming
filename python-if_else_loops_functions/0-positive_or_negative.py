d709e8d1178df5b9d8c4d4520c513730016e5f03
Bobson02 <global-m.bonheur@alustudent.com>
2024-03-29 14:42:22 +0200

    Modify: adding spaces between two strings

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10, 10)
 def check_number():
     if number > 0:
-        print(f"{number}Is positive")
+        print(f"{number} Is positive")
     elif  number < 0:
-        print(f"{number}Is negative")
+        print(f"{number} Is negative")
     else: 
-        print(f"{number}Is zero")
+        print(f"{number} Is zero")
 
 check_number()
