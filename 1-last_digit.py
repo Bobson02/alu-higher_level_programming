1bf8ea13db5b9a89ca25ea8937188dd1a704e794
Bobson02 <global-m.bonheur@alustudent.com>
2024-04-03 10:10:39 +0200

    fx: negatative number of last digit

--- 1-last_digit.py
+++ 1-last_digit.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-abs_num = abs(number % 10)
+abs_num = number % 10
 
 def num_printer():
 
-    if number < 0 and abs_num < 6 and abs_num!= 0:
-        abs_num =* -1 
+    if number < 0 and abs_num < 6 and abs_num != 0:
         print(f"The last digit of {number} is -{abs_num} and is less than 6 and not 0.")
-    elif number > 0 and abs_num < 6 and abs_num!= 0:
-        abs_num =* 1
+    elif number > 0 and abs_num < 6 and abs_num != 0:
         print(f"Last digit of {number} is {abs_num} and is less than 6 and not 0.")
-    elif abs_num > 6 and abs_num!= 0:
+    elif abs_num > 6 and abs_num != 0:
         print(f"Last digit of {number} is {abs_num}, which is greater than 6 and not 0.")
     elif abs_num == 6:
         print(f"The last digit of {number} is 6, and it is equal to 6 but not 0.")
