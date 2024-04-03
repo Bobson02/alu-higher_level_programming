5f7a8cd54cb6d4a1e382a05cda79ddbaa449bace
Bobson02 <global-m.bonheur@alustudent.com>
2024-04-03 10:20:05 +0200

    fix: string display

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-abs_num = abs(number) % 10
-def num_printer():
-
-    if number < 0: 
-        print(f"The last digit of {number} is -{abs_num} and is less than 6 and not 0.")
-    elif number > 0 and abs_num < 6 and abs_num != 0:
-        print(f"Last digit of {number} is {abs_num} and is less than 6 and not 0.")
-    elif abs_num > 6 and abs_num != 0:
-        print(f"Last digit of {number} is {abs_num}, and is greater than  6 and not 0.")
-    elif abs_num == 6:
-        print(f"The last digit of {number} is 6, and it is equal to 6 but not 0.")
-       
-    else:
-        print(f"Last digit of {number} is 0 and is 0")
-
-num_printer()
+if number < 0:
+    last_num = number % -10
+elif number >= 0:
+    last_num = number % 10
+if last_num > 5:
+    print(f"Last digit of {number} is {last_num} and is greater than 5")
+elif last_num == 0:
+    print(f"Last digit of {number} is {last_num} and is 0")
+else:
+    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
