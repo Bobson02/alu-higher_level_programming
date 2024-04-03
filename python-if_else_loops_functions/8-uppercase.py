f4e5e41773f1fcdf8757d1d8d480844d806449de
Bobson02 <global-m.bonheur@alustudent.com>
2024-04-03 20:36:57 +0200

    fix: error function removal

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def uppercase_checker():
     uppercase = __import__('8-uppercase').uppercase
     uppercase("best")
-    uppercase("Amezing  School with Sole learning system")
-
-uppercase_checker()
+    uppercase("amezing  School with Sole learning system")
