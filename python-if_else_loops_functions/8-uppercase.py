07917dd3346e60d45d857a6889fd640c2898a340
Bobson02 <global-m.bonheur@alustudent.com>
2024-04-03 20:39:12 +0200

    fix: adding loop to the program to enable string checking

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-    uppercase = __import__('8-uppercase').uppercase
-    uppercase("best")
-    uppercase("amezing  School with Sole learning system")
+def uppercase(str):
+    for i in str:
+        if ord("a") <= ord(i) <= ord("z"):
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
+    print()
