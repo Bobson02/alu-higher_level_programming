40a70e27188d3ebb875823d9968ea0de451d6688
Bobson02 <m.bonheur@alustudent.com>
2024-11-14 17:56:04 -0800

    fix:adding file perissions to the codebase

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let counter = 0;
+const apiUrl = process.argv[2];
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  try {
     const films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          counter += 1;
-        }
-      }
-    }
-    console.log(counter);
+    const wedgeId = '18';
+    const movieCount = films.reduce((count, film) => {
+      return film.characters.some(character => character.endsWith(`/${wedgeId}/`)) ? count + 1 : count;
+    }, 0);
+    console.log(movieCount);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
   }
 });
